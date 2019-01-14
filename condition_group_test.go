c25b7f75732785feb779b25bdbcb1337081c6c83
Nadim Bundhoo <nadim@lsl.digital>
2019-01-14 14:17:56 +0400

    Issue #1: Group conditions

--- condition_group_test.go
+++ condition_group_test.go
@@ -1,10 +1,11 @@
 package somesql_test
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
 
 	"github.com/lsldigital/somesql"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConditionGroup(t *testing.T) {
@@ -23,42 +24,104 @@ func TestConditionGroup(t *testing.T) {
 
 	tests := []testcase{
 		{
-			"AND",
-			[]somesql.Condition{},        //TODO write conditions
-			" AND (x AND y)",             //TODO write conditions
-			[]interface{}{"A", "B", "C"}, //TODO write values
+			"AND (1)",
+			[]somesql.Condition{
+				somesql.And("id", "=", "002fd6b1-f715-4875-838b-1546f27327df"),
+				somesql.And("status", "=", true),
+			},
+			"(id=? AND status=?)",
+			[]interface{}{"002fd6b1-f715-4875-838b-1546f27327df", true},
 			caseAnd,
 		},
 		{
-			"OR",
-			[]somesql.Condition{},        //TODO write conditions
-			" AND (x AND y)",             //TODO write conditions
-			[]interface{}{"A", "B", "C"}, //TODO write values
+			"AND (2)",
+			[]somesql.Condition{
+				somesql.Or("id", "=", "002fd6b1-f715-4875-838b-1546f27327df"),
+				somesql.And("status", "=", true),
+			},
+			"(id=? AND status=?)",
+			[]interface{}{"002fd6b1-f715-4875-838b-1546f27327df", true},
+			caseAnd,
+		},
+		{
+			"OR (1)",
+			[]somesql.Condition{
+				somesql.Or("id", "=", "002fd6b1-f715-4875-838b-1546f27327df"),
+				somesql.Or("status", "=", true),
+			},
+			"(id=? OR status=?)",
+			[]interface{}{"002fd6b1-f715-4875-838b-1546f27327df", true},
+			caseOr,
+		},
+		{
+			"OR (2)",
+			[]somesql.Condition{
+				somesql.And("id", "=", "002fd6b1-f715-4875-838b-1546f27327df"),
+				somesql.Or("status", "=", true),
+			},
+			"(id=? OR status=?)",
+			[]interface{}{"002fd6b1-f715-4875-838b-1546f27327df", true},
+			caseOr,
+		},
+		{
+			"AND JSONB (fields in 'data' only)",
+			[]somesql.Condition{
+				somesql.And("badge", "=", "video"),
+				somesql.And("has_video", "=", true),
+			},
+			`("data"->>'badge'=? AND "data"->>'has_video'::BOOLEAN=?)`,
+			[]interface{}{"video", true},
+			caseAnd,
+		},
+		{
+			"OR JSONB (fields in 'data' only)",
+			[]somesql.Condition{},
+			`("data"->>'badge'=? OR "data"->>'has_video'::BOOLEAN=?)`,
+			[]interface{}{"video", true},
+			caseOr,
+		},
+		{
+			"AND JSONB (fields in 'data' and predefined fields)",
+			[]somesql.Condition{},
+			`("data"->>'badge'=? AND type=?)`,
+			[]interface{}{"video", "article"},
+			caseAnd,
+		},
+		{
+			"OR JSONB (fields in 'data' and predefined fields)",
+			[]somesql.Condition{},
+			`("data"->>'badge'=? OR type=?)`,
+			[]interface{}{"video", "article"},
 			caseOr,
 		},
 	}
 
-	for _, tt := range tests {
+	for i, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
 			var (
-				sql    string
-				values interface{}
+				sql       string
+				values    interface{}
+				condition somesql.Condition
 			)
 
-			switch tt.caseType {
-			case caseAnd:
-				sql, values = somesql.AndGroup(tt.conditions...).AsSQL()
-			case caseOr:
-				sql, values = somesql.OrGroup(tt.conditions...).AsSQL()
+			for _, cond := range tt.conditions {
+				if tt.caseType == caseAnd {
+					condition = somesql.AndGroup(cond)
+				} else {
+					condition = somesql.OrGroup(cond)
+				}
 			}
 
-			if tt.sql != sql {
-				t.Errorf("Got %s, want %s", sql, tt.sql)
-			}
+			sql, values = condition.AsSQL()
 
-			if !reflect.DeepEqual(values, tt.values) {
-				t.Errorf("Values = %v, want %v", values, tt.values)
+			assert.Equal(t, tt.sql, sql, fmt.Sprintf("%d: SQL invalid", i+1))
+			assert.Equal(t, tt.values, values, fmt.Sprintf("%d: Values invalid", i+1))
+
+			if tt.caseType == caseAnd {
+				assert.Equal(t, somesql.AndCondition, condition.ConditionType(), fmt.Sprintf("%d: Condition type must be AND", i+1))
+			} else {
+				assert.Equal(t, somesql.OrCondition, condition.ConditionType(), fmt.Sprintf("%d: Condition type must be OR", i+1))
 			}
 		})
 	}
